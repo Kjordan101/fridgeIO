4cfcd6b4576f31424498287249309e2c4aa68266
Kevin Jordan <khernandez61600@Gmail.com>
2018-08-13 15:34:35 -0400

    New tables New input fields New get way to query New class names

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import jinja2
 from google.appengine.ext import ndb
 from google.appengine.api import urlfetch
 import json
-import model
+# import model
 from datetime import datetime
-from model import grocery
-from datetime import datetime
+from model import Grocery, Food
+# from datetime import datetime
 
 jinjaEnv = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -21,23 +21,35 @@ class homePage(webapp2.RequestHandler):
 
 class loadList(webapp2.RequestHandler):
     def get(self):
-        loadListPage = jinjaEnv.get_template('templates/loadlist.html')
-        past_lists = grocery.query().fetch()
-        
-        self.response.write(loadListPage.render({'lists' : past_lists}))
+        # loadListPage = jinjaEnv.get_template('templates/loadlist.html')
+        #testing fetch to database with mutipule tables
+        loadListPage = jinjaEnv.get_template('templates/debug.html')
+        past_lists = Grocery.query().fetch()
+        current_items = Food.query().fetch()
+        dict = {
+        "debug": past_lists
+        }
+        self.response.write(loadListPage.render({"debug":current_items}))
+        # self.response.write(loadListPage.render({'lists' : past_lists}))
 class newList(webapp2.RequestHandler):
     def get(self):
         newList = jinjaEnv.get_template('templates/newlist.html')
         self.response.write(newList.render())
     def post(self):
-        nameOfFood = self.request.get('food-name')
-        dateOfExpiration = self.request.get('expirationDate')
+        nameOfFood = self.request.get('food-name', allow_multiple=True)
+        dateOfExpiration = self.request.get('expirationDate',allow_multiple=True)
         pickYourTitle = self.request.get('title')
-        saveToDB = grocery(title = pickYourTitle, food = nameOfFood, expirationDate = (datetime.strptime(dateOfExpiration, '%Y-%m-%d')))
-        # pickYourTitle = self.request.get('title')
+
+        listOfFoods = []
+        for i in range(len(nameOfFood)):
+            food = Food(food = nameOfFood[i], expirationDate = datetime.strptime(dateOfExpiration[i],'%Y-%m-%d'))
+            foodKey = food.put()
+            listOfFoods.append(foodKey)
+
         # saveToDB = grocery(title = pickYourTitle, food = nameOfFood, expirationDate = (datetime.strptime(dateOfExpiration, '%Y-%m-%d')))
-        saveToDB.put()
-        #self.redirect('/newlist')
+        groceryToSave = Grocery(title = pickYourTitle, foods = listOfFoods)
+        groceryToSave.put()
+        self.redirect('/loadlist')
 
 app = webapp2.WSGIApplication([
     ('/', homePage),
--- model.py
+++ model.py
@@ -1,7 +1,10 @@
 from google.appengine.ext import ndb
 from datetime import datetime
 
-class grocery(ndb.Model):
+class Food(ndb.Model):
     food = ndb.StringProperty(required=True)
-    title = ndb.StringProperty(required=True)
     expirationDate = ndb.DateProperty(required=True)
+    #title = KeyProperty()
+class Grocery(ndb.Model):
+    title = ndb.StringProperty(required=True)
+    foods = ndb.KeyProperty(Food, repeated=True)
