62386cff66ff136442055add7b0cb2cff0f9b0e0
Kevin Jordan <khernandez61600@Gmail.com>
2018-08-14 09:09:52 -0400

    testing

--- model.py
+++ model.py
@@ -4,8 +4,6 @@ from datetime import datetime
 class Food(ndb.Model):
     food = ndb.StringProperty(required=True)
     expirationDate = ndb.DateProperty(required=True)
-    # title = ndb.KeyProperty(Grocery,repeated=True)
 class Grocery(ndb.Model):
     title = ndb.StringProperty(required=True)
     foods = ndb.KeyProperty(Food, repeated=True)
-    # foodString = ndb.StringProperty(Food, repeated=True)
